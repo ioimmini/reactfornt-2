d1fd11c42086d0f3b19c312b88bb41cabb0a82df
ioimmini <jumin0114@gmail.com>
2023-08-09 16:08:20 +0900

    input edit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,32 @@
 import React, {useState} from 'react';
-import './App.css';
-
-
-const RecordForm = ({numList, setNumList}) => {
-
-  const [num, setNum] = useState(0);
-
-  return (
-
-    <><div>
-
-      NUMBER : {num} </div><button onClick={() => setNum(num + 1)}>
-        increase
-      </button><button onClick={() => setNum(num - 1)}>
-        decrease
-      </button><button onClick={() => setNum(0)}>reset
-      </button>
-      <hr/>
-      <button onClick={()=> setNumList([...numList, num])}>list</button>
-      <button onClick={()=>setNumList([])}>reset</button></>
-  )
-
-}
-
-const ReocordList = ({numList}) => {
-  return (
-  <div>
-    <h2>list number</h2>
-    {numList.length > 0 ?<div>no record</div> :
-
-    <div>record</div>}
-    </div>
-  )
-}
-
 
 const App = () => {
 
-  const [numList, setNumList] = useState([]);
-  return(
-    <div style={{margin: "40px auto",
-                  width: "400px",
-                  textAlign: "center",
-                }}>
-        <RecordForm numList={numList} setNumList={setNumList}/>
-        <ReocordList numList={numList}/>
-    </div>
+  const [text, setText] = useState("11");
+  const [edit, setEdit] = useState(false);
 
-  )
+  let content = <div>
+    {text}<button onClick={()=> setEdit(true)}>edit</button>
+  </div>
+
+  if(edit) {
+    content = <div>
+    <input type="text"
+      value={text}
+      onChange={(e) => {
+        console.log(e.target.value);
+        setText(e.target.value);
+      }} 
+    />
+  <button onClick={()=>setEdit(false)}>edit</button>
+  </div>    
+  }
+
+  return (
+    <>
+      {content}
+    </>
+  );
 }
 
   
